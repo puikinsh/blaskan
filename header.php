2a116f781172cad94c3915d2f965277df3304448
Per Sandström <per@helloper.com>
2011-04-22 10:15:53 +0200

    Disqus styling

--- header.php
+++ header.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<!--[if lt IE 7 ]> <html <?php language_attributes(); ?> class="no-js ie ie6"> <![endif]-->
-<!--[if IE 7 ]>    <html <?php language_attributes(); ?> class="no-js ie ie7"> <![endif]-->
-<!--[if IE 8 ]>    <html <?php language_attributes(); ?> class="no-js ie ie8"> <![endif]-->
-<!--[if IE 9 ]>    <html <?php language_attributes(); ?> class="no-js ie ie9"> <![endif]-->
-<!--[if (gt IE 9)|!(IE)]><!--> <html <?php language_attributes(); ?> class="no-js"> <!--<![endif]-->
+<!--[if lt IE 7 ]> <html id="blaskan" <?php language_attributes(); ?> class="no-js ie ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html id="blaskan" <?php language_attributes(); ?> class="no-js ie ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html id="blaskan" <?php language_attributes(); ?> class="no-js ie ie8"> <![endif]-->
+<!--[if IE 9 ]>    <html id="blaskan" <?php language_attributes(); ?> class="no-js ie ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--> <html id="blaskan" <?php language_attributes(); ?> class="no-js"> <!--<![endif]-->
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
