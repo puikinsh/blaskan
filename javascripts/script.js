8a51a92505248c67a37866e732b02d39e898dfbf
Per Sandström <per@helloper.com>
2012-01-22 20:59:00 +0100

    Added GitHub feed

--- javascripts/script.js
+++ javascripts/script.js
@@ -20,4 +20,27 @@ function blaskan_forum_posts() {
     }
   });
 }
+
+function blaskan_github_feed() {
+  var feed = new google.feeds.Feed("https://github.com/persand/blaskan/commits/master.atom");
+  feed.setNumEntries(5);
+
+  feed.load(function(result) {
+    if (!result.error) {
+      var container = document.getElementById("blaskan-github-feed");
+      var html = '<ul>';
+
+      for (var i = 0; i < result.feed.entries.length; i++) {
+        var entry = result.feed.entries[i];
+        html += '<li><a href="' + entry.url + '">' + entry.title + '</a><br>' + entry.contentSnippet + '</li>';
+      }
+
+      html += '</ul>'
+
+      container.innerHTML = html;
+    }
+  });
+}
+
 google.setOnLoadCallback(blaskan_forum_posts);
+google.setOnLoadCallback(blaskan_github_feed);
