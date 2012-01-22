59b3d40297593392efa09557e6a576cd4f0fb253
Per Sandström <per@helloper.com>
2012-01-22 20:47:02 +0100

    Added correct forum RSS

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,7 +1,7 @@
 google.load("feeds", "1");
 
 function blaskan_forum_posts() {
-  var feed = new google.feeds.Feed("http://fastpshb.appspot.com/feed/1/fastpshb");
+  var feed = new google.feeds.Feed("http://wordpress.org/support/rss/tags/blaskan");
   feed.load(function(result) {
     if (!result.error) {
       var container = document.getElementById("blaskan-forum-posts");
