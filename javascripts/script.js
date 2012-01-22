a3696df5af17f6a3892e2a09134ebfb114d98242
Per Sandström <per@helloper.com>
2012-01-22 20:52:09 +0100

    Add posts data

--- javascripts/script.js
+++ javascripts/script.js
@@ -2,15 +2,21 @@ google.load("feeds", "1");
 
 function blaskan_forum_posts() {
   var feed = new google.feeds.Feed("http://wordpress.org/support/rss/tags/blaskan");
+  feed.setNumEntries(5);
+
   feed.load(function(result) {
     if (!result.error) {
       var container = document.getElementById("blaskan-forum-posts");
+      var html = '<ul>';
+
       for (var i = 0; i < result.feed.entries.length; i++) {
         var entry = result.feed.entries[i];
-        var div = document.createElement("div");
-        div.appendChild(document.createTextNode(entry.title));
-        container.appendChild(div);
+        html += '<li><a href="' + entry.url + '">' + entry.title + '</a><br>' + entry.contentSnippet + '</li>';
       }
+
+      html += '</ul>'
+
+      container.innerHTML = html;
     }
   });
 }
