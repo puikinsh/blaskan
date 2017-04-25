f5c1bd9507c3f45aa85af115151500c8b43a2d1c
Giucu <giucu91@yahoo.com>
2017-04-25 23:46:31 +0300

    #163 #164

--- header.php
+++ header.php
@@ -97,7 +97,9 @@
         <nav id="site-navigation" class="main-navigation container" role="navigation">
             <button class="menu-toggle" aria-controls="primary-menu"
                     aria-expanded="false"><?php esc_html_e( 'Menu', 'blaskan' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu', 'container_class' => 'blaskan-main-menu' ) ); ?>
+            <div class="blaskan-main-menu">
+			 <?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu', 'container' => false ) ); ?>
+            </div>
         </nav><!-- #site-navigation -->
 
         <!-- Custom Header -->
